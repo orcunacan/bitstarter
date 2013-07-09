095ee38cdee3b542abf5a98eca946166e8756849
Ubuntu <ubuntu@ip-172-31-2-16.us-west-2.compute.internal>
2013-07-09 19:45:44 +0000

    Unknown Content Test 1

--- web.js
+++ web.js
@@ -3,17 +3,20 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 var buffer = new Buffer(270);
 
-fs.readFileSync('index.html', 'utf-8', function (err, data) {
-  if (err) throw err;
-  console.log(data);
-  buffer.write(data);  
-});
-
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf-8', 0, 270));
+
+    fs.readFileSync('index.html', 'utf-8', function (err, data) {
+	if (err) throw err;
+	console.log(data);
+	buffer.write(data);
+    });
+
+
+    response.send(buffer.toString('utf-8', 0, 270));
+    console.log(data);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
