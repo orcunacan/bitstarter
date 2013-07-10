30d496581b26516817a447a9ee4847eb6a9e41ec
Ubuntu <ubuntu@ip-172-31-2-16.us-west-2.compute.internal>
2013-07-10 08:02:29 +0000

    HW3 Part 2

--- web.js
+++ web.js
@@ -1,21 +1,31 @@
 var express = require('express');
-var fs = require('fs');
+//var fs = require('fs');
 var app = express.createServer(express.logger());
 var buffer = new Buffer(200000);
 
+/*
 buffer.write(fs.readFileSync('index.html', 'utf-8'), 'utf-8');
 
 app.get('/', function(request, response) {
     response.send(buffer.toString('utf-8', 0, 27));
     console.log(data);
 });
+*/
+var http = require('http'),
+    fs = require('fs');
 
-// app.register('.html', require('jade'));
 
-var connect = require('connect');
-connect.createServer(
-    connect.static(__dirname)
-).listen(80);
+fs.readFile('./public/index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
+});
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
