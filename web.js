831b5dd5e3695f185ff9cd70da0b7af4b11b7bfc
Ubuntu <ubuntu@ip-172-31-2-16.us-west-2.compute.internal>
2013-07-10 09:53:41 +0000

    HW3 Part 2

--- web.js
+++ web.js
@@ -1,72 +1,22 @@
-var http = require("http"),
-url = require("url"),
-path = require("path"),
-fs = require("fs")
-port = process.argv[2] || 80;
-
-http.createServer(function(request, response) {
-
-  var uri = url.parse(request.url).pathname
-    , filename = path.join(process.cwd(), uri);
-
-  path.exists(filename, function(exists) {
-    if(!exists) {
-      response.writeHead(404, {"Content-Type": "text/plain"});
-      response.write("404 Not Found\n");
-      response.end();
-      return;
-    }
-
-    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
-
-    fs.readFile(filename, "binary", function(err, file) {
-      if(err) {        
-        response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(err + "\n");
-        response.end();
-        return;
-      }
-
-      response.writeHead(200);
-      response.write(file, "binary");
-      response.end();
-    });
-  });
-}).listen(parseInt(port, 10));
-
-console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
-
-
-/*var express = require('express');
+var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 var buffer = new Buffer(200000);
 var http = require('http');
 
-fs.readFile('index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(8000);
-    console.log(html);
-});
-
-
-
 buffer.write(fs.readFileSync('index.html', 'utf-8'), 'utf-8');
 
+http.createServer(function(request, response) {  
+    response.writeHeader(200, {"Content-Type": "text/html"});  
+    response.write(buffer.toString('utf-8'));  
+    response.end();  
+}).listen(8000);
+
 app.get('/', function(request, response) {
-    response.send(buffer.toString('utf-8', 0, 27));
-    response.end();
-    console.log(data);
+    response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
     console.log("Listening on " + port);
 });
-*/
