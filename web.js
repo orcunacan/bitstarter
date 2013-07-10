9ba89163a7189278cfa3158908dd1b3ab6e85619
Ubuntu <ubuntu@ip-172-31-2-16.us-west-2.compute.internal>
2013-07-10 11:03:41 +0000

    HW3 Part 2

--- web.js
+++ web.js
@@ -5,11 +5,12 @@ var buffer = new Buffer(200000);
 
 buffer.write(fs.readFileSync('index.html', 'utf-8'), 'utf-8');
 
-app.get('/', function(request, response) {
-    //response.send(buffer.toString('utf-8', 0, 27));
+/*app.get('/', function(request, response) {
+    response.send(buffer.toString('utf-8', 0, 27));
 });
+*/
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/'));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
