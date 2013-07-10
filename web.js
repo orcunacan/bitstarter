9263b0ecfaf1125a1a758e1cb25ff70062493331
Ubuntu <ubuntu@ip-172-31-2-16.us-west-2.compute.internal>
2013-07-10 09:19:45 +0000

    HW3 Part 2

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
-var express = require('express');
+//var express = require('express');
 var fs = require('fs');
-var app = express.createServer(express.logger());
+//var app = express.createServer(express.logger());
 var buffer = new Buffer(200000);
 var http = require('http');
 
